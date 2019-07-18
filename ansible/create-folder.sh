45f1b9afa5e80f7faa5d06f84587f82a7a4f3e0b
Andrew Shipilo <superdron97@yandex.ru>
2019-07-18 11:08:28 +0300

    Changed folder name in create-folder.sh to vol1

--- ansible/create-folder.sh
+++ ansible/create-folder.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Desc: create folder for PersistentVolume
-DIRNAME="vol2"
+DIRNAME="vol1"
 mkdir -p /mnt/disk/$DIRNAME
 chcon -Rt svirt_sandbox_file_t /mnt/disk/$DIRNAME
 chmod 777 /mnt/disk/$DIRNAME
